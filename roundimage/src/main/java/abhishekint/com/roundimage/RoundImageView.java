af5f3cd5ddc03cfe6995fb9ae2ce620f7d781da0
Abhishek Kumar <abhishekint16@gmail.com>
2018-05-10 15:03:15 +0530

    adding lines

--- roundimage/src/main/java/abhishekint/com/roundimage/RoundImageView.java
+++ roundimage/src/main/java/abhishekint/com/roundimage/RoundImageView.java
@@ -4,17 +4,20 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageView;
 
-public class RoundImageView extends View {
+public class RoundImageView extends ImageView {
 
     private int circleColor,circleTextColor;      //circle and text colors
     private String circleText;                    // circle text
-    private Paint circlePaint;                    // paint for drawing custom view
+    private Paint circlePaint,circlePaint2;                    // paint for drawing custom view
     private Context context;
     private AttributeSet attributeSet;
     public RoundImageView(Context context, @Nullable AttributeSet attrs) {
@@ -25,7 +28,8 @@ public class RoundImageView extends View {
     }
 
     private void initView() {
-        circlePaint=new Paint();                 //paint object for drawing in onDraw
+        circlePaint=new Paint();                   //paint object for drawing in onDraw
+        circlePaint2=new Paint();
         TypedArray  typedArray=context.getTheme().obtainStyledAttributes(attributeSet,R.styleable.RoundImageView,
                 0,0);      //This typed array will provide access to the attribute values.
         circleText=typedArray.getString(R.styleable.RoundImageView_circleText);
@@ -34,6 +38,7 @@ public class RoundImageView extends View {
         typedArray.recycle();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -46,6 +51,8 @@ public class RoundImageView extends View {
             radius=(height/2)-10;
         circlePaint.setColor(circleColor);
         canvas.drawCircle(width/2,height/2,radius,circlePaint);
+        /*circlePaint2.setColor(getResources().getColor(R.color.colorAccent));
+        canvas.drawOval(width/2,height/2,width/2,width/2,circlePaint2);*/
         circlePaint.setColor(circleTextColor);
         circlePaint.setTextAlign(Paint.Align.CENTER);
         canvas.drawText(circleText,width/2,height/2,circlePaint);
